7a99ab6b217198e0dde02e845b008c7bb52a8dca
Muhammad Abulkalam Asif <abulkalamasif1129@gmail.com>
2024-12-07 05:06:22 +0500

    minor animations

--- components/Screen01/Screen01.jsx
+++ components/Screen01/Screen01.jsx
@@ -1,37 +1,34 @@
-import { Screen01SVG } from "@/assets/images";
-import Image from "next/image";
+import Screen01SVGComp from "./Screen01SVGComp";
+
 export default function Screen01() {
-    return (
-        <section className="flex bg-main bg-no-repeat bg-cover items-center md:mb-[-90px]">
-            <div className="max-w-7xl md:pb-40 ~px-4/8 mx-auto flex flex-col-reverse items-center sm:flex-row sm:justify-between w-full">
-                <div className="sm:w-1/2 pb-10 flex flex-col justify-center gap-7 sm:pt-36">
-                    <h1 className="text-customYellow w-full ~text-3xl/5xl">
-                        TRANSFORMING YOUR VISION
-                        INTO AN EPIC CREATION
-                    </h1>
-                    <p className="~text-base/xl">
-                        We understand the expectations of gamers of today, thus creating games that involve the latest trends of technology and expertise including Metaverse/NFT/Blockchain
-                    </p>
-                    <ol className="list-disc pl-5 pt-3 ~text-base/xl">
-                        <li>NFT/Blockchain Games</li>
-                        <li>Mobile/Web Games</li>
-                        <li>PC/Console Games</li>
-                        <li>Metaverse/Augmented Reality/Virtual Reality</li>
-                    </ol>
-                    <div className="bg-gold-gradient w-fit py-1 px-3 rounded-[47px]">
-                        <button className="bg-custom-gradient px-10 py-5 rounded-[47px]">
-                            OUR PROJECTS
-                        </button>
-                    </div>
-                </div>
-                <div className="sm:w-1/2 ~pt-12/36">
-                    <Image
-                        src={Screen01SVG}
-                        alt="Screen01SVG"
-                        className="~w-[323]/[679] ~h-[323]/[679]"
-                    />
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section className="flex bg-main bg-no-repeat bg-cover items-center md:mb-[-90px]">
+      <div className="max-w-7xl md:pb-40 ~px-4/8 mx-auto flex flex-col-reverse items-center sm:flex-row sm:justify-between w-full">
+        <div className="sm:w-1/2 pb-10 flex flex-col justify-center gap-7 sm:pt-36">
+          <h1 className="text-customYellow w-full ~text-3xl/5xl">
+            TRANSFORMING YOUR VISION INTO AN EPIC CREATION
+          </h1>
+          <p className="~text-base/xl">
+            We understand the expectations of gamers of today, thus creating
+            games that involve the latest trends of technology and expertise
+            including Metaverse/NFT/Blockchain
+          </p>
+          <ol className="list-disc pl-5 pt-3 ~text-base/xl">
+            <li>NFT/Blockchain Games</li>
+            <li>Mobile/Web Games</li>
+            <li>PC/Console Games</li>
+            <li>Metaverse/Augmented Reality/Virtual Reality</li>
+          </ol>
+          <div className="bg-gold-gradient w-fit py-1 px-3 rounded-[47px]">
+            <button className="bg-custom-gradient px-10 py-5 rounded-[47px]">
+              OUR PROJECTS
+            </button>
+          </div>
+        </div>
+        <div className="sm:w-1/2 ~pt-12/36">
+          <Screen01SVGComp />
+        </div>
+      </div>
+    </section>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,12 +13,12 @@ module.exports = {
     extend: {
       keyframes: {
         scroll: {
-          '0%': { transform: 'translateY(0)' },
-          '90%': { transform: 'translateY(-50%)' }, // Adjust for seamless scrolling
+          "0%": { transform: "translateY(0)" },
+          "90%": { transform: "translateY(-50%)" }, // Adjust for seamless scrolling
         },
       },
       animation: {
-        scroll: 'scroll 10s linear infinite',
+        scroll: "scroll 10s linear infinite",
       },
       colors: {
         background: "var(--background)",
@@ -30,17 +30,62 @@ module.exports = {
       },
       backgroundImage: {
         main: "url('/images/Screen01/main.png')",
-        'custom-gradient': 'linear-gradient(89.97deg, #500200 7.1%, #BA0005 99.98%)',
-        'gold-gradient': 'linear-gradient(90deg, #DDA14B 0%, #FCE095 100%)',
-        'dark-gradient': 'linear-gradient(359.71deg, #191914 29.36%, #483E35 136.63%)',
-        'page03-gradient': 'linear-gradient(178.27deg, #3E0104 3.55%, #1E0303 88.75%)',
-        'page03-cardGradient': 'linear-gradient(180deg, #FDAA21 0%, #6B0C0C 100%)',
-        'page08-gradient': 'linear-gradient(359.26deg, #650206 0.63%, #FFD258 328.52%)',
+        "custom-gradient":
+          "linear-gradient(89.97deg, #500200 7.1%, #BA0005 99.98%)",
+        "gold-gradient": "linear-gradient(90deg, #DDA14B 0%, #FCE095 100%)",
+        "dark-gradient":
+          "linear-gradient(359.71deg, #191914 29.36%, #483E35 136.63%)",
+        "page03-gradient":
+          "linear-gradient(178.27deg, #3E0104 3.55%, #1E0303 88.75%)",
+        "page03-cardGradient":
+          "linear-gradient(180deg, #FDAA21 0%, #6B0C0C 100%)",
+        "page08-gradient":
+          "linear-gradient(359.26deg, #650206 0.63%, #FFD258 328.52%)",
         s5: "url('/images/screen05/bg.png')",
-        card1: 'url(/images/screen06/1.png)',
-        card2: 'url(/images/screen06/2.png)',
-        card3: 'url(/images/screen06/3.png)',
-        card4: 'url(/images/screen06/4.png)',
+        card1: "url(/images/screen06/1.png)",
+        card2: "url(/images/screen06/2.png)",
+        card3: "url(/images/screen06/3.png)",
+        card4: "url(/images/screen06/4.png)",
+      },
+      keyframes: {
+        leftRight: {
+          "0%": { transform: "translateX(0)" },
+          "50%": { transform: "translateX(-1rem)" }, // Adjust for seamless scrolling
+          "100%": { transform: "translateX(0)" },
+        },
+        playBtn: {
+          "0%": { transform: "scale(1)" },
+          "50%": { transform: "scale(1.3)" }, // Adjust for seamless scrolling
+          "100%": { transform: "scale(1)" },
+        },
+        anim1: {
+          "0%": { transform: "translate(0, 0)" },
+          "50%": { transform: "translate(-.3rem,.2rem)" }, // Adjust for seamless scrolling
+          "100%": { transform: "translate(0, 0)" },
+        },
+        anim2: {
+          "0%": { transform: "translate(0, 0)" },
+          "50%": { transform: "translate(.3rem,-.2rem)" }, // Adjust for seamless scrolling
+          "100%": { transform: "translate(0, 0)" },
+        },
+        anim3: {
+          "0%": { transform: "rotate(0)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+        dotDiagonal: {
+          "0%": { transform: "translate(0, 0)", opacity: 1 },
+          "50%": { transform: "translate(-4rem,-6rem)", opacity: 0.2 }, // Adjust for seamless scrolling
+          "100%": { transform: "translate(0, 0)", opacity: 1 },
+        },
+        
+      },
+      animation: {
+        leftRight: "leftRight 2s linear infinite",
+        playBtn: "playBtn 2s linear infinite",
+        anim1: "anim1 2s linear infinite",
+        anim2: "anim2 2s linear infinite",
+        anim3: "anim3 2s linear infinite",
+        dotDiagonal: "dotDiagonal 4s linear infinite",
       },
     },
     screens,
