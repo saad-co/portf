f95a6c45207e124903bce2f6eff4266a8c013bf6
rana saad <bitf21m041@pucit.edu.pk>
2024-12-07 19:13:29 +0500

    re

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,7 @@
 import fluid, { extract, screens, fontSize } from "fluid-tailwind";
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: {
     files: [
       "./pages/**/*.{js,ts,jsx,tsx,mdx}",
