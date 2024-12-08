3cbf8000ee4b059b77a07185544ecaf1c3540668
Muhammad Abulkalam Asif <abulkalamasif1129@gmail.com>
2024-12-08 19:11:34 +0500

    animation on logo added

--- app/layout.jsx
+++ app/layout.jsx
@@ -42,7 +42,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${Arame} ${ArameBold} ${ArameThin} antialiased`}>
+      <body className={`${Arame.className} antialiased`}>
         <Header />
         <AlertProvider>
           <AlertContainer />
