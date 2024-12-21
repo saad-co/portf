8deb6c1d13e826e7efd31e16d11070dcd27d9b8d
Muhammad Abulkalam Asif <abulkalamasif1129@gmail.com>
2024-12-21 14:53:44 +0500

    animation fixed

--- components/screen03/Screen03CardComp.jsx
+++ components/screen03/Screen03CardComp.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Screen03CardComp = ({ title, description, imgSrc }) => {
   return (
-    <div className="bg-page03-cardGradient w-full rounded-3xl">
+    <div className="bg-page03-cardGradient w-full h-min rounded-3xl">
       <div className="flex flex-col-reverse md:flex-row items-center justify-between p-6 md:p-8 gap-6 md:gap-12">
         <div className="w-full text-center md:text-start md:w-1/2 flex flex-col justify-center space-y-4 md:space-y-6 text-white">
           <h1 className="text-2xl md:text-4xl font-bold tracking-wide">
@@ -22,6 +22,7 @@ const Screen03CardComp = ({ title, description, imgSrc }) => {
               width={480}
               height={480}
               className="rounded-xl object-cover w-[480] h-[480]"
+              priority={true}
             />
           </div>
         </div>
